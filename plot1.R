53e57b652c54e7313ec89352fb15522dc9f58062
dhalldorson <35696448+dhalldorson@users.noreply.github.com>
2018-02-22 14:16:39 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,32 @@
+plot1<- function() {
+##******************************************************************************
+##This function reads in the household power consumption data, filters for the 
+##two dates required and creates the plot in a png file. 
+##
+##Requirements:
+##   Data file household_power_consuption.txt has already been downloaded and
+##   unzipped in the current working directory. 
+##Dependencies:
+##   dplyr and lubridate are already installed
+##******************************************************************************
+##Load libraries
+library(dplyr)
+library(lubridate)
+##Read in the entire file
+hpc<-read.table('household_power_consumption.txt', header=TRUE, sep=";", 
+                stringsAsFactors = FALSE, na.strings="?", 
+                col.names=c("Date","Time","globalactivepower", "globalreactivepower","voltage", 
+                            "globalintensity","submetering1","submetering2","submetering3"))
+hpc<-tbl_df(hpc)
+##Filter for the two dates needed and create a new variable for the DateTime
+hpc<-filter(hpc, (Date=="1/2/2007")|(Date=="2/2/2007"))
+#'hpc$Date<-dmy(hpc$Date)
+hpc<-mutate(hpc, DateTime=dmy_hms(paste(hpc$Date, hpc$Time)))
+
+##Open the graphics device, generate plot, and close the device
+##Plot1
+png(filename="plot1.png")
+hist(hpc$globalactivepower, col="Red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+dev.off()
+
+}
---
+++ plot4.R
@@ -0,0 +1,47 @@
+plot4<- function() {
+##******************************************************************************
+##This function reads in the household power consumption data, filters for the 
+##two dates required and creates the plot in a png file. 
+##
+##Requirements:
+##   Data file household_power_consuption.txt has already been downloaded and
+##   unzipped in the current working directory. 
+##Dependencies:
+##   dplyr and lubridate are already installed
+##******************************************************************************
+##Load libraries
+library(dplyr)
+library(lubridate)
+##Read in the entire file
+hpc<-read.table('household_power_consumption.txt', header=TRUE, sep=";", 
+                stringsAsFactors = FALSE, na.strings="?", 
+                col.names=c("Date","Time","globalactivepower", "globalreactivepower","voltage", 
+                            "globalintensity","submetering1","submetering2","submetering3"))
+hpc<-tbl_df(hpc)
+##Filter for the two dates needed and create a new variable for the DateTime
+hpc<-filter(hpc, (Date=="1/2/2007")|(Date=="2/2/2007"))
+#'hpc$Date<-dmy(hpc$Date)
+hpc<-mutate(hpc, DateTime=dmy_hms(paste(hpc$Date, hpc$Time)))
+
+##Open the graphics device, generate plot, and close the device
+##Plot4
+png(filename="plot4.png")
+par(mfrow=c(2,2))
+
+#sub graph 1
+with(hpc, plot(DateTime, globalactivepower, type="l", xlab="", ylab="Global Active Power"))
+
+#sub graph 2
+with(hpc, plot(DateTime, voltage, type="l", xlab="datetime", ylab="Voltage"))
+
+#sub graph 3
+with(hpc, plot(DateTime, submetering1, type="n", xlab="", ylab="Energy Sub Metering"))
+with(hpc, lines(DateTime, submetering1, col="black"))
+with(hpc, lines(DateTime, submetering2, col="red"))
+with(hpc, lines(DateTime, submetering3, col="purple"))
+legend("topright", lty=c(1,1,1), col=c("black", "red", "purple"), legend=c("Sub Metering 1", "Sub Metering 2", "Sub Metering 3"))
+
+#sub graph 4
+with(hpc, plot(DateTime, globalreactivepower, type="l", xlab="datetime", ylab="Global Reactive Power"))
+dev.off()
+}
